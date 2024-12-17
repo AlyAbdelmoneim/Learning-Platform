4ed7f7b71b8238bb2e036944027bace69d3f5256
Eyad3skr <eyad.abdulaal@student.giu-uni.de>
2024-12-17 19:21:44 +0200

    Second commit 3rd component

--- TestApp/Models/LearnersGoals.cs
+++
@@ -1,8 +0,0 @@
-public class LearnersGoal
-{
-    public int LearnerID { get; set; }
-    public int GoalID { get; set; }
-
-    // Additional properties for the join table
-    public DateTime DateAdded { get; set; }
-}
--- TestApp/Models/ViewModels/LoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TestApp.Models.ViewModels
-{
-    public class LoginViewModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
